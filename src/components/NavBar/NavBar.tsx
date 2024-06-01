9e9b1968f41d7c561ed1ad5905654371e424d42c
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2024-06-01 09:34:28 +0200

    feat(navigation): update text hover

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -71,7 +71,7 @@ export default function NavBar() {
                         className={classNames(
                           item.current
                             ? " text-sky-600 font-bold "
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            : "text-gray-400 hover:bg-sky-600 hover:text-white",
                           "rounded-md px-3 py-2 text-sm font-medium"
                         )}
                         aria-current={item.current ? "page" : undefined}
