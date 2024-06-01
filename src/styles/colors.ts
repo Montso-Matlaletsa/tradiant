43690ce536466bddff32b97cee938b19e3b7ede1
Montso-Matlaletsa <montso.matlaletsa@gmail.com>
2024-06-01 08:54:42 +0200

    feat(navigation): add colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,33 @@
+export default {
+  Primary: {
+    "primary-900": "#053358",
+    "primary-800": "#074C83",
+    "primary-600": "#0C7FDA",
+    "primary-700": "#0966AF",
+    "primary-500": "#2196F3",
+    "primary-400": "#48A8F5",
+    "primary-300": "#6EBBF7",
+    "primary-200": "#95CDF9",
+    "primary-100": "#BCDFFB",
+    "primary-50": "#CFE8FC",
+    "white-200": "#EFF2F4",
+  },
+  Gray: {
+    "gray-900": "#1E252B",
+    "gray-800": "#333F49",
+    "gray-700": "#485967",
+    "gray-600": "#5D7285",
+    "gray-500": "#768C9F",
+    "gray-400": "#91A3B2",
+    "gray-300": "#ACB9C5",
+    "gray-200": "#C7D0D8",
+    "gray-100": "#E2E7EB",
+    "gray-50": "#EFF2F4",
+    "white-100": "#FFFFFF",
+  },
+  Support: {
+    green: "#007F5F",
+    red: "#BA2D0B",
+    yellow: "#F49D37",
+  },
+};
